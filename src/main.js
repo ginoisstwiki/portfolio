2aeac5e9970ad930330f3e206b89c80a69fd1056
ginoisstwiki <otisconfidicktus@gmail.com>
2023-10-21 02:21:25 +0200

    added light-mode

--- src/main.js
+++ src/main.js
@@ -5,4 +5,13 @@ import '../components/hero.css';
 import '../components/about.css';
 import '../components/featured.css';
 import '../components/work.css';
+import '../components/contact.css';
+import '../components/footer.css';
+import '../components/mobile-nav.css';
 import '../styles/utils.css';
+
+import mobileNav from './utils/mobile-nav';
+import darkMode from './utils/dark-mode';
+
+mobileNav();
+darkMode();
